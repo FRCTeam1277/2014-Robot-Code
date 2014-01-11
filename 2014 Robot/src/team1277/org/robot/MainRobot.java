3156c6b0fc0e3870e9fdde6f142771d4d349484a
itsZN <itsZN@github.com>
2014-01-10 20:50:53 -0500

    Initial Robot Code

---
+++ 2014 Robot/src/team1277/org/robot/MainRobot.java
@@ -0,0 +1,46 @@
+package team1277.org.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class MainRobot extends IterativeRobot {
+	
+	public MainRobot() {
+		super();
+	}
+	
+	@Override
+	public void robotInit() {
+		
+	}
+	
+	@Override
+	public void disabledInit() {
+		
+	}
+	
+	@Override
+	public void disabledPeriodic() {
+		
+	}
+	
+	@Override
+	public void teleopInit() {
+		
+	}
+	
+	@Override
+	public void teleopPeriodic() {
+		
+	}
+	
+	@Override
+	public void autonomousInit() {
+		
+	}
+	
+	@Override
+	public void autonomousPeriodic() {
+		
+	}
+	
+}
