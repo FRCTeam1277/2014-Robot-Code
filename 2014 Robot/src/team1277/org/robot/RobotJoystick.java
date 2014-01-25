8bd2b0d01813b16e18dd26467128977923c9c496
itsZN <itsZN@github.com>
2014-01-24 21:10:05 -0500

    acutally did xbox

---
+++ 2014 Robot/src/team1277/org/robot/RobotJoystick.java
@@ -0,0 +1,58 @@
+package team1277.org.robot;
+
+
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.Joystick;
+
+public class RobotJoystick {
+	private Joystick stick;
+	private Hand hand;
+	private RobotJoystick() {}
+	
+	public RobotJoystick(int port) {
+		stick = new Joystick(port);
+	}
+	
+	public RobotJoystick(int port, Hand hand) {
+		stick = new Joystick(port);
+	}
+	public RobotJoystick(Joystick joy, Hand hand) {
+		stick = joy;
+	}
+	
+	public double getX() {
+		if (hand.equals(Hand.kLeft)) {
+			return stick.getRawAxis(1);
+		}
+		else {
+			return stick.getRawAxis(4);
+		}
+	}
+	
+	public double getY() {
+		if (hand.equals(Hand.kLeft)) {
+			return stick.getRawAxis(1);
+		}
+		else {
+			return stick.getRawAxis(4);
+		}
+	}
+	
+	public double getZ() {
+		return stick.getRawAxis(3);
+	}
+	
+	public double getThrottle() {
+		return stick.getRawAxis(4);
+	}
+	
+	public boolean getRawButton(int button) {
+		return stick.getRawButton(button);
+	}
+	
+	public double getRawAxis(int axis) {
+		return stick.getRawAxis(axis);
+	}
+	
+	
+}
